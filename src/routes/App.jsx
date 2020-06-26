190d070db4c8209e2fa9b6b3362f6af827e1e4aa
Felichz <andersson.fx@gmail.com>
2020-06-26 01:50:48 -0300

    Added base url env variable

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -10,14 +10,14 @@ import Player from '../components/Player';
 import NotFound from '../containers/NotFound';
 
 const App = () => (
-    <BrowserRouter basename={config.baseUrl}>
+    <BrowserRouter basename={process.env.BASE_URL}>
         <Layout>
             <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route exact path="/login" component={Login}/>
-                <Route exact path="/register" component={Register}/>
-                <Route exact path="/play/:id" component={Player}/>
-                <Route component={NotFound}/>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/play/:id" component={Player} />
+                <Route component={NotFound} />
             </Switch>
         </Layout>
     </BrowserRouter>
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,64 @@
+const webpack = require('webpack');
 const path = require('path');
 
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.html$/,
-        loader: "html-loader"
-      },
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif|jpeg)$/,
-        use: {
-          'loader': 'url-loader',
-          options: {
-            name: 'assets/[hash].[ext]'
-          }
-        }
-      }
-    ]
-  },
-  devServer: {
-    historyApiFallback: true // Prevents default browser refresh on form submission and link change
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "./assets/[name].css"
-    })
-  ]
-};
+const BASE_URL = process.env.BASE_URL || '/';
+
+module.exports = (env) => ({
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: env.BASE_URL || '/',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif|jpeg)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        name: 'edovideo/assets/[hash].[ext]',
+                    },
+                },
+            },
+        ],
+    },
+    devServer: {
+        historyApiFallback: true, // Prevents default browser refresh on form submission and link change
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './public/index.html',
+            filename: './index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: './assets/[name].css',
+        }),
+        new webpack.DefinePlugin({
+            'process.env.BASE_URL': JSON.stringify(env.BASE_URL),
+        }),
+    ],
+});
