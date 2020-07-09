94de0df6aa290ed940a958f0646a58431e3e98a1
Felichz <andersson.fx@gmail.com>
2020-07-09 00:12:16 -0300

    removed unused import

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import config from '../config';
 
 import Layout from '../containers/Layout';
 import Home from '../containers/Home';
